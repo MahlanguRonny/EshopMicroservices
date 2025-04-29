43f41620ed124728ed263f69e08f0145fd8e4f48
Ronny.Mahlangu@zeda.co.za <Ronny.Mahlangu@zeda.co.za>
2025-04-29 16:58:48 +0200

    Created store basket handler class with it's endpoint

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,26 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class StoreBasketCommandHandler
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+            //TODO: store basket in the datbase (user Martern  - if exist = update, if )
+            //TODO: update cache
+
+            return new StoreBasketResult("swn");
+        }
     }
 }
