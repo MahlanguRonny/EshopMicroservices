94e8d0b3e390d58243331343d6b0aafbc0df8a71
Ronny.Mahlangu@zeda.co.za <Ronny.Mahlangu@zeda.co.za>
2025-05-09 21:02:13 +0200

    generate new grpc api(templating is grp on creation), install ef tools and generated initial db using ef migrations

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -2,7 +2,7 @@
 {
     public class Coupon
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string ProductName { get; set; } = default!;
         public string Description { get; set; } = default!;
         public int Amount { get; set; }
