b2d119a53e2dfb8bd0152e0f61caa69542a21f07
Ronny.Mahlangu@zeda.co.za <Ronny.Mahlangu@zeda.co.za>
2025-06-23 16:49:21 +0200

    Seeding data changes

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrustructure;
+using Ordering.Infrustructure.Data.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,4 +15,10 @@ builder.Services
 var app = builder.Build();
 
 // configure the HTTP request pipleline
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 app.Run();
