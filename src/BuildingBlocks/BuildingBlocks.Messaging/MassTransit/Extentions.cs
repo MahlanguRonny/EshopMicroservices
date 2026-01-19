96d224cc53610d43a72cc0e5c6e61e91e5b441fc
Ronny.Mahlangu@zeda.co.za <Ronny.Mahlangu@zeda.co.za>
2026-01-19 18:49:48 +0200

    Register message broker extention of the publisher with the DI

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -7,7 +7,7 @@ namespace BuildingBlocks.Messaging.MassTransit
 {
     public static class Extentions
     {
-        public static IServiceCollection AddressBroker
+        public static IServiceCollection AddMessageBroker
             (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
         {
             services.AddMassTransit(config =>
